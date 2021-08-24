144c0be595700d45c9247acded652b0aa3a061ca
mohammadyousaf0 <abdul2015@outlook.dk>
2021-08-24 09:26:54 +0200

    Add solutiont to assignment Niveau 2 01

---
+++ Opgaver/Niveau2/01/js/index.js
@@ -0,0 +1,6 @@
+// "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
+
+// N2 - 01
+document.addEventListener("DOMContentLoaded", () => {
+ 
+}); // Afslutter: DOMContentLoaded
